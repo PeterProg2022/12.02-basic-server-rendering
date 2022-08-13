4889e7bbdb18095cef8e509de5cf0cdf6f22c793
Peter Prog <peter.prog@yahoo.com>
2022-08-13 18:36:29 +0800

    render on browser

--- ui/server/render.js
+++ ui/server/render.js
@@ -1,12 +1,12 @@
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 
-const About = require('./About.js').default;
-const template = require('./template.js');
+// const About = require('./About').default;
+const template = require('./template');
 
 function render(req, res) {
   const body = ReactDOMServer.renderToString(
-    React.createElement(About),
+    // React.createElement(About),
   );
   res.send(template(body));
 }
--- ui/server/uiserver.js
+++ ui/server/uiserver.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
-const render = require('./render.js');
+const render = require('./render');
 
 const app = express();
 
@@ -17,7 +17,7 @@ if (enableHMR && (process.env.NODE_ENV !== 'production')) {
   const devMiddleware = require('webpack-dev-middleware');
   const hotMiddleware = require('webpack-hot-middleware');
 
-  const config = require('../webpack.config.js');
+  const config = require('../webpack.config');
   config.entry.app.push('webpack-hot-middleware/client');
   config.plugins = config.plugins || [];
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
